2e1a68d40f8c4b9c267236d8324b8650bf4751d9
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-01 11:14:14 +0700

    Moved all state into one place (score changes)

--- scoreboard/src/components/Counter.js
+++ scoreboard/src/components/Counter.js
@@ -1,31 +1,16 @@
 import React from 'react';
 
-class Counter extends React.Component {
-    state = { 
-      score: 0 
-    };
-    
-    incrementScore = () => {
-      this.setState( prevState => ({
-        score: prevState.score + 1
-      }));
-    }
-  
-    decrementScore = () => {
-      this.setState( prevState => ({
-        score: prevState.score - 1
-      }));
-    }
-  
-    render() {
-      return (
+const Counter = (props) => {
+
+    let index = props.index;
+
+    return (
         <div className="counter">
-          <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-          <span className="counter-score">{ this.state.score }</span>
-          <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+            <button className="counter-action decrement" onClick={ () => props.changeScore(index, -1)}> - </button>
+            <span className="counter-score">{props.score}</span>
+            <button className="counter-action increment" onClick={ () => props.changeScore(index, 1)}> + </button>
         </div>
-      );
-    }
-  }
-  
-  export default Counter
+    );
+}
+
+export default Counter
